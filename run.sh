20a0f3772de7d760707636c56996dd85fb15a203
Toxikuu <toxikuu@users.noreply.github.com>
2024-12-15 15:17:36 -0600

    added a run script

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# TODO: test this
+
+if grep -A1 tuunfm ~/.config/tuun/config.toml | grep -q true; then
+  if ! pgrep -x tuunfm; then
+    tuunfm &
+  else
+    echo "tuunfm is already running!" >&2
+  fi
+fi
+
+if ! pgrep -x tuun; then
+  tuun
+else
+  echo "tuun is already running!" >&2
+fi
