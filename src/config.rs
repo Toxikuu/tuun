993c9f94697f74e5c98362c10541f34db89f92d5
Toxikuu <toxikuu@users.noreply.github.com>
2024-12-15 15:18:53 -0600

    config improvements

--- src/config.rs
+++ src/config.rs
@@ -10,10 +10,12 @@ pub struct Config {
     pub lastfm: LastFMConfig,
     pub discord: DiscordConfig,
     pub general: GeneralConfig,
+    pub tuunfm: TuunFMConfig,
 }
 
 #[derive(Deserialize)]
 pub struct LastFMConfig {
+    pub used: bool,
     pub apikey: String,
     pub secret: String,
     pub user: String,
@@ -22,19 +24,31 @@ pub struct LastFMConfig {
 
 #[derive(Deserialize)]
 pub struct DiscordConfig {
+    pub used: bool,
     pub client_id: String,
 }
 
 #[derive(Deserialize)]
 pub struct GeneralConfig {
+    pub verbose: bool,
+    pub socket: String,
+    pub playlist: String,
     pub polling_rate: u64,
 }
 
-pub fn load_config() -> Config {
-    let home_dir = dirs::home_dir().expect("Couldn't find home directory");
-    let config_path = home_dir.join(".config/tuun/config.toml");
-    let config_str = fs::read_to_string(config_path).expect("Couldn't find config.toml");
-    let config: Config = toml::de::from_str(&config_str).expect("Invalid config");
+#[derive(Deserialize)]
+pub struct TuunFMConfig {
+    pub used: bool,
+    pub link: String,
+}
 
-    config
+impl Config {
+    pub fn load() -> Self {
+        let home_dir = dirs::home_dir().expect("Couldn't find home directory");
+        let config_path = home_dir.join(".config/tuun/config.toml");
+        let config_str = fs::read_to_string(config_path).expect("Couldn't find config.toml");
+        let config: Config = toml::de::from_str(&config_str).expect("Invalid config");
+
+        config
+    }
 }
