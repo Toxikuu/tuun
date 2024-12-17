101d93a0779b40eb537ec2e2371bb1ad8c9ae31f
Toxikuu <toxikuu@users.noreply.github.com>
2024-12-17 14:34:22 -0600

    rofi now shuffles

--- q.sh
+++ q.sh
@@ -15,11 +15,15 @@ fi
 
 mapfile -t DISPLAY_NAMES < <(printf '%s\n' "${FULL_PATHS[@]}" | awk -F'/' '{print $NF}')
 
-SELECTED_NAMES=$(printf '%s\n' "${DISPLAY_NAMES[@]}" | rofi -matching fuzzy -i -sort -theme-str 'window {width: 50%;}' -theme-str 'listview {columns: 1; lines: 15;}' -dmenu -multi-select -p "Queue songs:")
+SHUFFLED_DISPLAY_NAMES=$(printf '%s\n' "${DISPLAY_NAMES[@]}" | shuf)
+
+SELECTED_NAMES=$(printf '%s\n' "${SHUFFLED_DISPLAY_NAMES[@]}" | rofi -matching fuzzy -i -sort -theme-str 'window {width: 50%;}' -theme-str 'listview {columns: 1; lines: 15;}' -dmenu -multi-select -p "Queue songs:")
 if [[ -z "$SELECTED_NAMES" ]]; then
     echo "No songs selected"
     exit 0
 fi
+
+SELECTED_NAMES=$(echo "$SELECTED_NAMES" | shuf)
 
 while IFS= read -r SELECTED_NAME; do
     for i in "${!DISPLAY_NAMES[@]}"; do
